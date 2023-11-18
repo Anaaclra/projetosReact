b9bdff4369d4795dba192e845ef76f0ddbb09378
anaclara <anaclaraa02pereira@gmail.com>
2023-11-18 12:21:31 -0300

    Correção de rotas

--- src/config/key.js
+++ src/config/key.js
@@ -1,1 +1,1 @@
-export const APIKEY = "2991304cd890b434863fd6bfba05e94c";
+export const APIKey = "2991304cd890b434863fd6bfba05e94c";
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,7 @@
 import { Container } from "./style";
 import { Movielist } from "./style";
 import { Movie } from "./style";
-import { APIKEY } from "../config/key";
+import { APIKey } from "../config/key";
 import { useState, useEffect } from "react";
 
 
@@ -14,7 +14,7 @@ function Home() {
 
     useEffect(() => {
 
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=2991304cd890b434863fd6bfba05e94c&language=pt-BR&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=pt-BR&page=1`)
             .then(response => response.json())
             .then(data => { 
                 setMovies(data.results) 
@@ -50,7 +50,7 @@ function Home() {
                     movies.map(movie => {
                         return (
                             <Movie>
-                                <a href="https://www.google.com">
+                                <a href="#">
                                     <img src={`${img_path}${movie.poster_path}`} alt={movie.title} />
                                 </a>
                                 <span>{movie.title}</span>
